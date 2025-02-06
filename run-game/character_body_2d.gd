e29554328c6566b9ae497c50a69df491d0beda31
Dustin Groh <dustngroh@gmail.com>
2025-02-05 19:34:36 -0500

    Add test ball object, setup Discord webhook

--- run-game/character_body_2d.gd
+++ run-game/character_body_2d.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var push_force = 80.0
 
 
 func _physics_process(delta: float) -> void:
@@ -23,3 +24,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	for i in get_slide_collision_count():
+		var c = get_slide_collision(i)
+		if c.get_collider() is RigidBody2D:
+			c.get_collider().apply_central_impulse(-c.get_normal() * push_force)
