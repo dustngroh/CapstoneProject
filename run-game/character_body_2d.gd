a1d8d6147624abf6601790d77df7f374fb058c6e
Dustin Groh <dustngroh@gmail.com>
2025-02-09 16:20:36 -0500

    Add acceleration/deceleration to Player. Add Project Input Map.

--- run-game/character_body_2d.gd
+++ run-game/character_body_2d.gd
@@ -1,30 +1,28 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-var push_force = 80.0
+#const SPEED = 300.0
+#const JUMP_VELOCITY = -400.0
+@export var push_force = 200.0
+@export var speed = 1000
+@export var jump_speed = -1000
+@export var gravity = 2000
+@export_range(0.0, 1.0) var friction = 0.1
+@export_range(0.0 , 1.0) var acceleration = 0.08
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	velocity.y += gravity * delta
+	var dir = Input.get_axis("walk_left", "walk_right")
+	if dir != 0:
+		velocity.x = lerp(velocity.x, dir * speed, acceleration)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = lerp(velocity.x, 0.0, friction)
 
 	move_and_slide()
-	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
+
 	for i in get_slide_collision_count():
 		var c = get_slide_collision(i)
 		if c.get_collider() is RigidBody2D:
