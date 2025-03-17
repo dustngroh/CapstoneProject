7d0c803a753d3c57373bbd640138b107cb57fccf
Dustin Groh <dustngroh@gmail.com>
2025-03-17 04:13:48 -0400

    Add reset threshold to endless run to avoid floating point physics errors

--- run-game/scenes/levels/obstacle_spawner.gd
+++ run-game/scenes/levels/obstacle_spawner.gd
@@ -7,5 +7,6 @@ func _ready():
 		var chance = randf() # Random spawn chance
 		if chance <= 0.2:
 			var obstacle = obstacle_scenes.pick_random().instantiate()
+			#obstacle.add_to_group("movable_objects")
 			add_child(obstacle)
 			obstacle.position = Vector2(randf_range(100, 500), 0)  # Randomize position
--- run-game/scenes/levels/test_levels/level_spawner.gd
+++ run-game/scenes/levels/test_levels/level_spawner.gd
@@ -3,18 +3,23 @@ extends Node2D
 @export var level_chunks: Array[PackedScene] 
 @export var chunk_spawn_distance := 1920.0  # How far ahead to spawn new chunks
 @export var max_chunks := 5 
+@export var RESET_THRESHOLD = 30720  # Reset world when the player crosses this point
+@export var shift_amount = 30720
 
 var active_chunks = []  # List of currently spawned chunks
 var player
+var camera : Camera2D
 
 func _ready():
 	player = get_tree().get_nodes_in_group("player")[-1] 
+	camera = player.get_node("Camera2D")
 	spawn_initial_chunks()
 	UIManager.hide_level_ui() # Make sure level UI is not visible
 	#UIManager.get_node("FadeLayer").visible = false # For testing purposes
 
 func _process(_delta):
 	check_and_spawn_chunks()
+	recenter_world_if_needed()
 
 # Spawn a few starting chunks
 func spawn_initial_chunks():
@@ -45,6 +50,21 @@ func check_and_spawn_chunks():
 	if player.position.x > last_chunk_end_x - chunk_spawn_distance:
 		spawn_new_chunk(Vector2(last_chunk_end_x, 0))
 
+# Recenter world to avoid floating point precision errors in physics beyond threshold
+func recenter_world_if_needed():
+	if player.position.x > RESET_THRESHOLD:
+		
+		# Move player back
+		player.position.x -= shift_amount
+
+		# Move all active chunks back
+		for chunk in active_chunks:
+			chunk.position.x -= shift_amount
+		
+		# Force camera to remain centered left of player
+		player.position.x -= 500 # Move player left an extra amount
+		camera.force_update_scroll() # Force camera onto them
+		player.position.x += 500 # Move player back right same amount
 
 func _on_main_menu_button_pressed() -> void:
 	var game = get_tree().root.get_node("Game")
