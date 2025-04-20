fde445d2037416130e826a41295f6c2700b41e06
Dustin Groh <dustngroh@gmail.com>
2025-04-20 00:01:50 -0400

    Add respawning to speed_boost_item, change sprite
    Add a Timer to speed_boost_item to allow respawning the object after a specified amount of time.
    Change sprite to new texture.

--- run-game/scripts/speed_boost_item.gd
+++ run-game/scripts/speed_boost_item.gd
@@ -2,10 +2,12 @@ extends Area2D
 
 @export var speed_boost_factor = 1.5
 @export var speed_boost_duration = 3.0
+@export var respawn_time = 5.0
+@onready var respawn_timer = $Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	respawn_timer.wait_time = respawn_time
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,4 +18,12 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.increase_speed(speed_boost_factor, speed_boost_duration)
-		queue_free()  # Remove the item after collection
+		#queue_free()  # Remove the item after collection
+		visible = false
+		set_deferred("monitoring", false)
+		respawn_timer.start()
+
+
+func _on_timer_timeout() -> void:
+	visible = true
+	monitoring = true
