890c2cf731394e8e0429710dba7b9363080f3b04
Dustin Groh <dustngroh@gmail.com>
2025-04-28 16:24:02 -0400

    Update player collision, cleanup files.

--- run-game/scenes/entities/characters/player/character_body_2d.gd
+++ run-game/scenes/entities/characters/player/character_body_2d.gd
@@ -82,6 +82,7 @@ func _physics_process(delta: float) -> void:
 	# Determine animation state
 	if !is_on_floor():  
 		$AnimatedSprite2D.play("jumping")  # Play jump animation when airborne
+		$AnimatedSprite2D.speed_scale = 1.0
 	#elif Input.is_action_pressed("hold_down"):
 		#$AnimatedSprite2D.play("down")  # Play crouch animation
 	elif dir != 0:
